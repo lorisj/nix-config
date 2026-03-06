b20a7eca21492bd23fd2dec296c6228d7d739552
lorisj <loris@bluenotehealth.com>
2026-03-05 17:10:33 -0800

    theme for nvim (home specific)

--- modules/home/editor/nixvim.nix
+++ modules/home/editor/nixvim.nix
@@ -4,13 +4,54 @@
   lib,
   ...
 }:
+let
+  # Captured from flake config (outer scope); inner scope only has home-manager config.
+  nixvimBase = config.flake.modules.nixvim.base;
+in
 {
   flake.homeModules.nixvim =
-    { pkgs, ... }:
+    { config, pkgs, ... }:
     let
+      # base16 colorscheme expects #color; nix-colors palette has hex without #
+      colors =
+        with config.colorScheme.palette;
+        lib.mapAttrs (_name: value: "#" + value) {
+          inherit
+            base00
+            base01
+            base02
+            base03
+            base04
+            base05
+            base06
+            base07
+            base08
+            base09
+            base0A
+            base0B
+            base0C
+            base0D
+            base0E
+            base0F
+            ;
+        };
+
+      paletteModule = {
+        colorschemes.base16 = {
+          enable = true;
+          colorscheme = colors;
+        };
+      };
+
+      # makeNixvimWithModule expects a single module, not a "merge" value.
+      nixvimModule =
+        base: palette:
+        { ... }:
+        { config = lib.mkMerge [ base palette ]; };
+
       nixvim = inputs.nixvim.legacyPackages.${pkgs.stdenv.hostPlatform.system}.makeNixvimWithModule {
         inherit pkgs;
-        module = config.flake.modules.nixvim.base;
+        module = nixvimModule nixvimBase paletteModule;
       };
     in
     {

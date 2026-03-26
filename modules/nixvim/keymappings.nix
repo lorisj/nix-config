748c94dc836cc4d0bc4f5e02596099743d988e04
lorisj <loris@bluenotehealth.com>
2026-03-26 12:41:03 -0700

    keybinds

--- modules/nixvim/keymappings.nix
+++ modules/nixvim/keymappings.nix
@@ -4,5 +4,12 @@
       mapleader = " ";
       maplocalleader = " ";
     };
+    keymaps = [
+      # Move windows w/ ctrl
+      { mode = ["n" "t"]; key = "<Tab>h"; action = "<C-\\><C-n><C-w>h"; options = { desc = "move to left window"; silent = true; }; }
+      { mode = ["n" "t"]; key = "<Tab>j"; action = "<C-\\><C-n><C-w>j"; options = { desc = "move to below window"; silent = true; }; }
+      { mode = ["n" "t"]; key = "<Tab>k"; action = "<C-\\><C-n><C-w>k"; options = { desc = "move to above window"; silent = true; }; }
+      { mode = ["n" "t"]; key = "<Tab>l"; action = "<C-\\><C-n><C-w>l"; options = { desc = "move to right window"; silent = true; }; }
+    ];
   };
 }

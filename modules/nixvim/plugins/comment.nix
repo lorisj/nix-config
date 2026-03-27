3b930334942609936cf69eb2a788f30e8c5d18de
lorisj <loris@bluenotehealth.com>
2026-03-27 15:31:32 -0700

    comment

--- modules/nixvim/plugins/comment.nix
+++ modules/nixvim/plugins/comment.nix
@@ -2,7 +2,7 @@
   flake.modules.nixvim.base.plugins.comment = {
     enable = true;
     settings = {
-      opleader.line = "<leader>'";
+      opleader.line = "<leader>/";
       toggler.line = "<leader>/";
     };
   };

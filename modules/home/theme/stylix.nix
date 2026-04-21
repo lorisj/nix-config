6b46bb6fcdd5a6f98a3289b04b2e66f418797125
lorisj <loris@bluenotehealth.com>
2026-04-21 09:53:32 -0700

    wallpaper

--- modules/home/theme/stylix.nix
+++ modules/home/theme/stylix.nix
@@ -4,12 +4,6 @@
   flake.homeModules.stylix =
     { config, lib, ... }:
     {
-      options = {
-        wallpaperPath = lib.mkOption {
-          description = "Path to wallpaper to use w/ stylix";
-          type = lib.types.path;
-        };
-      };
       config = {
         stylix.base16Scheme = config.colorScheme.palette;
         stylix.image = config.wallpaperPath;
---
+++ modules/home/theme/wallpaper.nix
@@ -0,0 +1,11 @@
+{ flake, ... }:
+{
+  flake.homeModules.wallpaper =
+    { lib, ... }:
+    {
+      options.wallpaperPath = lib.mkOption {
+        type = lib.types.path;
+        description = "Path to the wallpaper image (e.g. macOS desktop picture, stylix.image).";
+      };
+    };
+}

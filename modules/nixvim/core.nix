746730225c357c1abdfc6b67d10978e979344689
lorisj <loris@bluenotehealth.com>
2026-04-28 19:56:28 -0700

    more wraps

--- modules/nixvim/core.nix
+++ modules/nixvim/core.nix
@@ -1,49 +1,51 @@
 { ... }:
 {
   flake.nixvimModules.core = { ... }: {
-    plugins = {
-      # Lazy loading
-      #lz-n.enable = true;
+    config = {
+      plugins = {
+        # Lazy loading
+        #lz-n.enable = true;
+      };
+      # Disable netrw to prevent flash on startup
+      globals = {
+        loaded_netrw = false;
+        loaded_netrwPlugin = false;
+      };
+      plugins.mini-icons.enable = true;
+
+      plugins.web-devicons.enable = true;
+      opts = {
+        updatetime = 100; # faster completion
+        swapfile = false;
+
+        # line numbers
+        relativenumber = true;
+        number = true; # current line number absolute
+
+        cursorline = true;
+        ignorecase = true; # ignore case for search query
+        undofile = true;
+        autoread = true;
+
+        # tab options
+        tabstop = 4;
+        shiftwidth = 4; # number of spaces for each step of autoindent
+        autoindent = true;
+        expandtab = true;
+      };
+      autoCmd = [
+        {
+          event = [ "FocusGained" "BufEnter" "CursorHold" "CursorHoldI" ];
+          pattern = [ "*" ];
+          command = "if mode() != 'c' | checktime | endif";
+        }
+        # prevents claude from being listed in the buffer list
+        {
+          event = [ "TermOpen" ];
+          pattern = [ "*bin/claude" ];
+          command = "set nobuflisted";
+        }
+      ];
     };
-    # Disable netrw to prevent flash on startup
-    globals = {
-      loaded_netrw = false;
-      loaded_netrwPlugin = false;
-    };
-    plugins.mini-icons.enable = true;
-
-    plugins.web-devicons.enable = true;
-    opts = {
-      updatetime = 100; # faster completion
-      swapfile = false;
-
-      # line numbers
-      relativenumber = true;
-      number = true; # current line number absolute
-
-      cursorline = true;
-      ignorecase = true; # ignore case for search query
-      undofile = true;
-      autoread = true;
-
-      # tab options
-      tabstop = 4;
-      shiftwidth = 4; # number of spaces for each step of autoindent
-      autoindent = true;
-      expandtab = true;
-    };
-    autoCmd = [
-      {
-        event = [ "FocusGained" "BufEnter" "CursorHold" "CursorHoldI" ];
-        pattern = [ "*" ];
-        command = "if mode() != 'c' | checktime | endif";
-      }
-      # prevents claude from being listed in the buffer list
-      {
-        event = [ "TermOpen" ];
-        pattern = [ "*bin/claude" ];
-        command = "set nobuflisted";
-      }
-    ];
   };
 }
--- modules/nixvim/keymappings.nix
+++ modules/nixvim/keymappings.nix
@@ -1,33 +1,35 @@
 { ... }:
 {
   flake.nixvimModules.keymappings = { ... }: {
-    globals = {
-      mapleader = " ";
-      maplocalleader = " ";
-    };
-    keymaps = [
-      # Move windows w/ ctrl
-      { mode = ["n" "t"]; key = "<tab>h"; action = "<C-\\><C-n><C-w>h"; options = { desc = "move to left window"; silent = true; }; }
-      { mode = ["n" "t"]; key = "<tab>j"; action = "<C-\\><C-n><C-w>j"; options = { desc = "move to below window"; silent = true; }; }
-      { mode = ["n" "t"]; key = "<tab>k"; action = "<C-\\><C-n><C-w>k"; options = { desc = "move to above window"; silent = true; }; }
-      { mode = ["n" "t"]; key = "<tab>l"; action = "<C-\\><C-n><C-w>l"; options = { desc = "move to right window"; silent = true; }; }
-      # Focus main buffer (anything that is not neotree, etc.)
-      {
-        mode = ["n" "t"];
-        key = "<tab>m";
-        action.__raw = ''
-          function()
-            for _, win in ipairs(vim.api.nvim_list_wins()) do
-              local buf = vim.api.nvim_win_get_buf(win)
-              if vim.bo[buf].buftype == "" and vim.bo[buf].filetype ~= "neo-tree" then
-                vim.api.nvim_set_current_win(win)
-                return
+    config = {
+      globals = {
+        mapleader = " ";
+        maplocalleader = " ";
+      };
+      keymaps = [
+        # Move windows w/ ctrl
+        { mode = ["n" "t"]; key = "<tab>h"; action = "<C-\\><C-n><C-w>h"; options = { desc = "move to left window"; silent = true; }; }
+        { mode = ["n" "t"]; key = "<tab>j"; action = "<C-\\><C-n><C-w>j"; options = { desc = "move to below window"; silent = true; }; }
+        { mode = ["n" "t"]; key = "<tab>k"; action = "<C-\\><C-n><C-w>k"; options = { desc = "move to above window"; silent = true; }; }
+        { mode = ["n" "t"]; key = "<tab>l"; action = "<C-\\><C-n><C-w>l"; options = { desc = "move to right window"; silent = true; }; }
+        # Focus main buffer (anything that is not neotree, etc.)
+        {
+          mode = ["n" "t"];
+          key = "<tab>m";
+          action.__raw = ''
+            function()
+              for _, win in ipairs(vim.api.nvim_list_wins()) do
+                local buf = vim.api.nvim_win_get_buf(win)
+                if vim.bo[buf].buftype == "" and vim.bo[buf].filetype ~= "neo-tree" then
+                  vim.api.nvim_set_current_win(win)
+                  return
+                end
               end
             end
-          end
-        '';
-        options = { desc = "Focus main buffer"; silent = true; };
-      }
-    ];
+          '';
+          options = { desc = "Focus main buffer"; silent = true; };
+        }
+      ];
+    };
   };
 }

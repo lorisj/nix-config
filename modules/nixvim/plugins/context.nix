5e0031d30e3bca7bd8732376dba6ad7cb89b20d1
lorisj <loris@bluenotehealth.com>
2026-04-28 18:17:49 -0700

    minor

--- modules/nixvim/plugins/context.nix
+++ modules/nixvim/plugins/context.nix
@@ -1,15 +1,17 @@
 { ... }:
 {
-  flake.nixvimModules.plugins.context = { ... }: {
-    plugins.treesitter-context.enable = true;
-    keymaps = [
-      {
-        mode = "n";
-        key = "<leader>tc";
-        action = "<cmd>TSContext toggle<cr>";
-        options.desc = "Toggle treesitter context";
-        options.silent = true;
-      }
-    ];
-  };
+  flake.nixvimModules.plugins.context =
+    { ... }:
+    {
+      plugins.treesitter-context.enable = true;
+      keymaps = [
+        {
+          mode = "n";
+          key = "<leader>tc";
+          action = "<cmd>TSContext toggle<cr>";
+          options.desc = "Toggle treesitter context";
+          options.silent = true;
+        }
+      ];
+    };
 }

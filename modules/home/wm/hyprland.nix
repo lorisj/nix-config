e3f3ec70f0fe3586ba9945e0227c51160f41cce8
lorisj <loris@bluenotehealth.com>
2026-05-05 19:44:08 -0700

    hyprland fix option def

--- modules/home/wm/hyprland.nix
+++ modules/home/wm/hyprland.nix
@@ -1,17 +1,33 @@
 { ... }:
 {
   flake.homeModules.wm.hyprland =
-    { pkgs, lib, osConfig, ... }:
-    let
-      displayScaling = lib.attrByPath [
-        "os"
-        "display"
-        "hyprland"
-        "displayScaling"
-      ] 1 osConfig;
-    in
+    { pkgs, lib, osConfig, config, ... }:
     {
-      config = lib.mkIf pkgs.stdenv.hostPlatform.isLinux {
+      options.wm.hyprland.enable = lib.mkOption {
+        type = lib.types.bool;
+        readOnly = true;
+        default = lib.attrByPath [
+          "os"
+          "display"
+          "hyprland"
+          "enabled"
+        ] false osConfig;
+        description = "Mirrors NixOS `os.display.hyprland.enabled` from `osConfig` when Home Manager runs on NixOS.";
+      };
+
+      options.wm.hyprland.displayScaling = lib.mkOption {
+        type = lib.types.int;
+        readOnly = true;
+        default = lib.attrByPath [
+          "os"
+          "display"
+          "hyprland"
+          "displayScaling"
+        ] 1 osConfig;
+        description = "Mirrors NixOS `os.display.hyprland.displayScaling` from `osConfig`.";
+      };
+
+      config = lib.mkIf (pkgs.stdenv.hostPlatform.isLinux && config.wm.hyprland.enable) {
         wayland.windowManager.hyprland = {
           enable = true;
           package = null;
@@ -23,7 +39,7 @@
             "$fileManager" = "thunar";
             "$menu" = "wofi --show drun";
             monitor = [
-              ",preferred,auto,${builtins.toString displayScaling}"
+              ",preferred,auto,${builtins.toString config.wm.hyprland.displayScaling}"
             ];
             exec-once = [
               "${pkgs.waybar}/bin/waybar"

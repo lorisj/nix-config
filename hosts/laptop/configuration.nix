fac84c6bc89aa4e82a9053b7ca015f21b1e4ab71
lorisj <lorisjautakas@gmail.com>
2026-05-05 19:18:24 -0700

    grub + other nixos options

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -9,6 +9,33 @@
     modules = [
       self.osModules.hardwareConfigurations.laptop
       self.osModules.default
+      {
+        time.timeZone = "America/Los_Angeles";
+        i18n.defaultLocale = "en_US.UTF-8";
+
+        i18n.extraLocaleSettings = {
+          LC_ADDRESS = "en_US.UTF-8";
+          LC_IDENTIFICATION = "en_US.UTF-8";
+          LC_MEASUREMENT = "en_US.UTF-8";
+          LC_MONETARY = "en_US.UTF-8";
+          LC_NAME = "en_US.UTF-8";
+          LC_NUMERIC = "en_US.UTF-8";
+          LC_PAPER = "en_US.UTF-8";
+          LC_TELEPHONE = "en_US.UTF-8";
+          LC_TIME = "en_US.UTF-8";
+        };
+
+        boot.loader.systemd-boot.enable = true;
+        boot.loader.efi.canTouchEfiVariables = true;
+        services.printing.enable = true;
+
+        system.stateVersion = "25.11";
+
+        nixos.display.hyprland.enabled = true;
+        #nixos.display.hyprland.displayScaling = 2;
+        nixos.display.hyprland.laptopKeybinds = true;
+
+      }
     ];
   };
 }

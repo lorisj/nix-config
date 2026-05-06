ac8fb311d7194852779702a078401706ec2bed07
lorisj <loris@bluenotehealth.com>
2026-05-05 19:08:26 -0700

    users.users

--- modules/system/nixos/home-manager.nix
+++ modules/system/nixos/home-manager.nix
@@ -1,10 +1,24 @@
-{ inputs, ... }:
+{ inputs, lib, self, ... }:
+let
+  userNames = lib.sort lib.lessThan (lib.attrNames self.userConfig);
+in
 {
   flake.osModules.home-manager =
-    { ... }:
+    { lib, ... }:
     {
       imports = [
         inputs.home-manager.nixosModules.default
       ];
+      config = {
+        users.users = builtins.listToAttrs (
+          builtins.map (userName: {
+            name = userName;
+            value = {
+              isNormalUser = true;
+              home = "/home/${userName}";
+            };
+          }) userNames
+        );
+      };
     };
 }

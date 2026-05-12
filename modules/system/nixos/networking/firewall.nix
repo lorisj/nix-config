90a417e18688a36364a47d63000ce6f7fd21d8e2
lorisj <lorisjautakas@gmail.com>
2026-05-11 23:03:13 -0700

    revert

--- modules/system/nixos/networking/firewall.nix
+++ modules/system/nixos/networking/firewall.nix
@@ -9,10 +9,11 @@
       config = lib.mkIf config.os.networking.firewall.enabled {
         networking.firewall = {
           enable = true;
-          extraInputRules = ''
-            ip saddr 192.168.50.74 tcp dport 3001 accept
-            ip saddr != 192.168.50.74 tcp dport 3001 drop
-          '';
+          allowedTCPPorts = [ 3001 ];
+          # extraInputRules = ''
+          #   ip saddr 192.168.50.74 tcp dport 3001 accept
+          #   ip saddr != 192.168.50.74 tcp dport 3001 drop
+          # '';
         };
       };
     };

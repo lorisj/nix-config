a17e0c75680b18e1353d5d7544a03e9d48c13e17
lorisj <loris@bluenotehealth.com>
2026-05-16 12:12:46 -0700

    ssh

---
+++ modules/system/nixos/networking/ssh.nix
@@ -0,0 +1,47 @@
+{ lib, self, ... }:
+let
+  userNames = lib.sort lib.lessThan (lib.attrNames self.userConfig);
+
+  defaultKeys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICeZI2pBSxv1fs6V8hAe5DDyHSUT4UCcQZTXK9sLPxwt"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOgykMePA2aCBXm2uktQSnI4p4f0++eWZ5hYF80vtrMo"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFZU98KS18bZSgmD7xsKozlldpneJW8zlgzi5HvxWog0"
+  ];
+in
+{
+  flake.osModules.networking.ssh =
+    { config, lib, ... }:
+    let
+      cfg = config.os.networking.ssh;
+    in
+    {
+      options = {
+        os.networking.ssh.enabled = lib.mkEnableOption "ssh";
+
+        os.networking.ssh.allowedUsers = lib.mkOption {
+          type = lib.types.listOf lib.types.str;
+          default = userNames;
+          description = "Users allowed to SSH into this host.";
+        };
+      };
+
+      config = lib.mkIf cfg.enabled {
+        services.openssh = {
+          enable = true;
+          settings = {
+            PasswordAuthentication = false;
+            KbdInteractiveAuthentication = false;
+            PermitRootLogin = "no";
+            AllowUsers = cfg.allowedUsers;
+          };
+        };
+
+        users.users = builtins.listToAttrs (
+          builtins.map (userName: {
+            name = userName;
+            value.openssh.authorizedKeys.keys = defaultKeys;
+          }) cfg.allowedUsers
+        );
+      };
+    };
+}

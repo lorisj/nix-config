d79cfe07248b8143042315c27d2cf49a0fd2794d
lorisj <loris@bluenotehealth.com>
2026-06-01 16:57:06 -0700

    vi + cache

--- modules/home/terminal/shells.nix
+++ modules/home/terminal/shells.nix
@@ -6,9 +6,17 @@
       config = {
         programs.bash = {
           enable = true;
+          initExtra = ''
+            set -o vi
+            bind -m vi-insert '"\C-r": reverse-search-history'
+          '';
         };
         programs.zsh = {
           enable = true;
+          initContent = ''
+            bindkey -v
+            bindkey -M viins '^R' history-incremental-search-backward
+          '';
         };
       };
     };
--- modules/system/shared/nix/settings.nix
+++ modules/system/shared/nix/settings.nix
@@ -7,11 +7,13 @@
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
         "https://cuda-maintainers.cachix.org"
+        "https://cache.garnix.io"
       ];
 
       nix.settings.trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       ];
 
       nix.settings.experimental-features = [

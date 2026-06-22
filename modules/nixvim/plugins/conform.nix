8a60f0780c9fa9fcff8e7b91ed6586f767490296
lorisj <loris@bluenotehealth.com>
2026-06-22 13:11:48 -0700

    haskell

--- modules/nixvim/plugins/conform.nix
+++ modules/nixvim/plugins/conform.nix
@@ -1,48 +1,56 @@
 { ... }:
 {
-  flake.nixvimModules.plugins.conform = { ... }: {
+  flake.nixvimModules.plugins.conform = { pkgs, ... }: {
     config = {
-    plugins.conform-nvim = {
-      enable = true;
+      extraPackages = [
+        pkgs.haskellPackages.cabal-fmt
+        pkgs.haskellPackages.fourmolu
+      ];
 
-      settings = {
-        notify_on_error = true;
+      plugins.conform-nvim = {
+        enable = true;
 
-        formatters_by_ft = {
-          javascript = [ "biome" ];
-          typescript = [ "biome" ];
-          javascriptreact = [ "biome" ];
-          typescriptreact = [ "biome" ];
-          json = [ "biome" ];
-          jsonc = [ "biome" ];
+        settings = {
+          notify_on_error = true;
+
+          formatters_by_ft = {
+            javascript = [ "biome" ];
+            typescript = [ "biome" ];
+            javascriptreact = [ "biome" ];
+            typescriptreact = [ "biome" ];
+            json = [ "biome" ];
+            jsonc = [ "biome" ];
+            haskell = [ "fourmolu" ];
+            lhaskell = [ "fourmolu" ];
+            cabal = [ "cabal_fmt" ];
+          };
+
+          format_on_save = ''
+            function(bufnr)
+                local disable_filetypes = { c = true, cpp = true }
+                if disable_filetypes[vim.bo[bufnr].filetype] then 
+                    return nil
+                else
+                    return {
+                        timeout_ms = 5000,
+                        lsp_format = "fallback",
+                    }
+                end
+            end
+          '';
         };
-
-        format_on_save = ''
-          function(bufnr)
-              local disable_filetypes = { c = true, cpp = true }
-              if disable_filetypes[vim.bo[bufnr].filetype] then 
-                  return nil
-              else
-                  return {
-                      timeout_ms = 5000,
-                      lsp_format = "fallback",
-                  }
-              end
-          end
-        '';
       };
-    };
-    keymaps = [
-      {
-        mode = "n";
-        key = "<leader>F";
-        action.__raw = ''
-          function()
-              require('conform').format { async = true, lsp_fallback = true }
-          end
-        '';
-      }
-    ];
+      keymaps = [
+        {
+          mode = "n";
+          key = "<leader>F";
+          action.__raw = ''
+            function()
+                require('conform').format { async = true, lsp_fallback = true }
+            end
+          '';
+        }
+      ];
     };
   };
 }
--- modules/nixvim/plugins/lsp.nix
+++ modules/nixvim/plugins/lsp.nix
@@ -3,96 +3,116 @@
 {
   flake.nixvimModules.plugins.lsp = { ... }: {
     config = {
-    plugins.lsp = {
-    enable = true;
-    inlayHints = true;
-
-    # Configure how diagnostics are displayed
-    onAttach = ''
-      vim.diagnostic.config({
-        virtual_text = true,
-        signs = true,
-        underline = true,
-        update_in_insert = false,
-        severity_sort = true,
-      })
-      -- show hover information in border
-      vim.keymap.set("n", "H", function()
-        vim.lsp.buf.hover({ border = "rounded" })
-      end, { buffer = bufnr, desc = "show hover information", silent = true })
-    '';
-
-    servers = {
-      gopls = {
+      plugins.lsp = {
         enable = true;
-        autostart = true;
-      };
-      ts_ls.enable = true;
-      cssls.enable = true;
-      tailwindcss.enable = true;
-      html.enable = true;
-      pyright.enable = true;
-      bashls.enable = true;
-      rust_analyzer = {
-        enable = true;
-        # these should be installed by project (in devshell)
-        installCargo = false;
-        installRustc = false;
-      };
-      hls.enable = true;
-      hls.installGhc = true;
-      # leanls is handled by lean.nvim plugin, not lspconfig
-      nixd = {
-        enable = true;
-        settings =
-          let
-            flake2 = ''builtins.getFlake "github:lorisj/private-nixos-config"'';
-          in
-          {
-            nixpkgs = {
-              expr = "import ${flake2}.inputs.nixpkgs {}";
-            };
-            # formatting = {
-            #   command = [ "${lib.getExe pkgs.nixfmt-rfc-style}" ];
-            # };
-            options = {
-              # TODO: fix
-              nixos.expr = "${flake2}.nixosConfigurations.server.options";
+        inlayHints = true;
 
+        # Configure how diagnostics are displayed
+        onAttach = ''
+          vim.diagnostic.config({
+            virtual_text = true,
+            signs = true,
+            underline = true,
+            update_in_insert = false,
+            severity_sort = true,
+          })
+          -- show hover information in border
+          vim.keymap.set("n", "H", function()
+            vim.lsp.buf.hover({ border = "rounded" })
+          end, { buffer = bufnr, desc = "show hover information", silent = true })
+        '';
+
+        servers = {
+          gopls = {
+            enable = true;
+            autostart = true;
+          };
+          ts_ls.enable = true;
+          cssls.enable = true;
+          tailwindcss.enable = true;
+          html.enable = true;
+          pyright.enable = true;
+          bashls.enable = true;
+          rust_analyzer = {
+            enable = true;
+            # these should be installed by project (in devshell)
+            installCargo = false;
+            installRustc = false;
+          };
+          hls = {
+            enable = true;
+            installGhc = true;
+            filetypes = [
+              "haskell"
+              "lhaskell"
+              "cabal"
+            ];
+            settings = {
+              haskell = {
+                formattingProvider = "fourmolu";
+                plugin = {
+                  cabal.globalOn = true;
+                  ghcide-code-actions-fill-holes.globalOn = true;
+                  ghcide-code-actions-imports-exports.globalOn = true;
+                  hlint.globalOn = true;
+                  importLens.globalOn = true;
+                  retrie.globalOn = true;
+                };
+              };
             };
           };
-      };
-    };
-    # https://github.com/Ahwxorg/nixvim-config/blob/master/config/modules/plugins/lsp.nix
+          # leanls is handled by lean.nvim plugin, not lspconfig
+          nixd = {
+            enable = true;
+            settings =
+              let
+                flake2 = ''builtins.getFlake "github:lorisj/private-nixos-config"'';
+              in
+              {
+                nixpkgs = {
+                  expr = "import ${flake2}.inputs.nixpkgs {}";
+                };
+                # formatting = {
+                #   command = [ "${lib.getExe pkgs.nixfmt-rfc-style}" ];
+                # };
+                options = {
+                  # TODO: fix
+                  nixos.expr = "${flake2}.nixosConfigurations.server.options";
 
-    keymaps = {
-      silent = true;
-      lspBuf = {
-        "<leader>rn" = {
-          action = "rename";
-          desc = "rename symbol";
+                };
+              };
+          };
         };
-        "<leader>a" = {
-          action = "code_action";
-          desc = "code actions";
-        };
-      };
-      diagnostic = {
-        "<leader>e" = {
-          action = "open_float";
-          desc = "show diagnostic in float";
-        };
-        "[" = {
-          action = "goto_prev";
-          desc = "previous diagnostic";
-        };
-        "]" = {
-          action = "goto_next";
-          desc = "next diagnostic";
+        # https://github.com/Ahwxorg/nixvim-config/blob/master/config/modules/plugins/lsp.nix
+
+        keymaps = {
+          silent = true;
+          lspBuf = {
+            "<leader>rn" = {
+              action = "rename";
+              desc = "rename symbol";
+            };
+            "<leader>a" = {
+              action = "code_action";
+              desc = "code actions";
+            };
+          };
+          diagnostic = {
+            "<leader>e" = {
+              action = "open_float";
+              desc = "show diagnostic in float";
+            };
+            "[" = {
+              action = "goto_prev";
+              desc = "previous diagnostic";
+            };
+            "]" = {
+              action = "goto_next";
+              desc = "next diagnostic";
+            };
+          };
         };
       };
     };
-  };
-  };
   };
 }

edf71c541492d87205eb7e671ab7c2badc58ba00
lorisj <loris@bluenotehealth.com>
2026-07-07 16:28:15 -0700

    barbar updates

--- modules/nixvim/plugins/barbar.nix
+++ modules/nixvim/plugins/barbar.nix
@@ -4,6 +4,54 @@
     config = {
       plugins.barbar = {
         enable = true;
+        settings = {
+          animation = true;
+          clickable = true;
+          insert_at_end = true;
+          maximum_length = 24;
+          maximum_padding = 2;
+          minimum_length = 4;
+          minimum_padding = 1;
+          no_name_title = "scratch";
+          tabpages = false;
+          sidebar_filetypes = {
+            "neo-tree" = {
+              event = "BufWipeout";
+              text = "files";
+              align = "center";
+            };
+          };
+          icons = {
+            button = "×";
+            modified.button = "●";
+            pinned = {
+              button = "󰐃";
+              filename = true;
+              separator.right = "";
+            };
+            scroll = {
+              left = "‹";
+              right = "›";
+            };
+            separator = {
+              left = "";
+              right = "";
+            };
+            current.separator = {
+              left = "";
+              right = "";
+            };
+            visible.separator = {
+              left = "";
+              right = "";
+            };
+            inactive.separator = {
+              left = "";
+              right = "";
+            };
+            separator_at_end = false;
+          };
+        };
         keymaps = {
           next.key = "K";
           previous.key = "J";

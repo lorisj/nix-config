16f4f4b6ca5b46bcec260e64a2c45c5bdd1609e6
lorisj <loris@bluenotehealth.com>
2026-07-08 17:15:28 -0700

    remove unused

--- flake.nix
+++ flake.nix
@@ -32,42 +32,15 @@
   outputs =
     inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = (inputs.nix-helpers.lib.find-all-files-by-name ./hosts "configuration.nix")
-      ++ (inputs.nix-helpers.lib.find-nix-files ./modules)
-      ++ (inputs.nix-helpers.lib.find-nix-files ./users);
+      imports =
+        (inputs.nix-helpers.lib.find-all-files-by-name ./hosts "configuration.nix")
+        ++ (inputs.nix-helpers.lib.find-nix-files ./modules)
+        ++ (inputs.nix-helpers.lib.find-nix-files ./users);
       systems = [
         "x86_64-linux"
         "aarch64-linux"
         "aarch64-darwin"
         "x86_64-darwin"
       ];
-      perSystem =
-        {
-          config,
-          self',
-          inputs',
-          pkgs,
-          system,
-          ...
-        }:
-        {
-          # Per-system attributes can be defined here. The self' and inputs'
-          # module parameters provide easy access to attributes of the same
-          # system.
-          # TODO: replace specialArgs with this overlay, currently doesn't work for some reason
-          #_module.args.pkgs = import inputs.nixpkgs {
-          #  inherit system;
-          #  overlays = [
-          #    inputs.nix-helpers.overlays.nhLib-overlay
-          #  ];
-          #  config.allowUnfree = true;
-          #};
-        };
-
-      flake = {
-        # The usual flake attributes can be defined here, including system-
-        # agnostic ones like nixosModule and system-enumerating ones, although
-        # those are more easily expressed in perSystem.
-      };
     };
 }

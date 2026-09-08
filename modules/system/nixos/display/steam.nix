3cfe6943efa6860af01a4ea964b57c2280fdaf27
Dillan Pribak <dmpribak@umich.edu>
2026-09-07 21:36:34 -0700

    extra arg

--- modules/system/nixos/display/steam.nix
+++ modules/system/nixos/display/steam.nix
@@ -6,7 +6,7 @@
       cfg = config.os.display.steam;
       bigPicture = pkgs.writeShellScript "steam-bigpicture" ''
         exec ${lib.getExe pkgs.gamescope} --steam -e \
-          -W ${toString cfg.width} -H ${toString cfg.height} \
+          -W ${toString cfg.width} -H ${toString cfg.height} --force-composite \
           -- steam -tenfoot -pipewire-dmabuf
       '';
     in

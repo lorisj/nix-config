7b218e60612e055b24f9a099fbd7f0e9d5261e52
lorisj <loris@bluenotehealth.com>
2026-09-25 14:48:12 -0700

    fix warning

--- modules/home/theme/stylix.nix
+++ modules/home/theme/stylix.nix
@@ -9,6 +9,8 @@
         stylix.image = config.wallpaperPath;
         stylix.targets.hyprlock.enable = false;
         stylix.targets.dunst.enable = true;
+        # The launcher uses Wofi; avoid configuring the unused Rofi target.
+        stylix.targets.rofi.enable = false;
         # stylix.targets.firefox.profileNames = [ "default" ];
         # stylix.targets.firefox.enable = true;
         # stylix.targets.firefox.colorTheme.enable = true;
